a8da3d9433c82821efed3ee225f0e08c8feb0d35
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:33 -0300

    Arrays Bidimencionais

---
+++ ConsoleArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                {{"O Pequeno","sim"},{"O Grande","não"}};
+
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+            {
+                var Nomelivro = listaDeLivros[i,0];
+                var disponivel = listaDeLivros[i, 1];
+
+                Console.WriteLine($"Nome do Livro:{Nomelivro} disponivel:{disponivel}.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
