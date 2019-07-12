fb594e9287cb82ef187d0e5af6b18e2edb190f93
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 14:07:17 -0300

    Login de Maior Idade

---
+++ Login de Maior idade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_de_Maior_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informa o seu Nome: ");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine("Informa a sua idade: ");
+            var idade = int.Parse(Console.ReadLine());
+
+            if (idade <18)
+                Console.WriteLine("Você não está autorizado a compra de bebidas alcoolicas!");
+            else
+                 Console.WriteLine("Beba por que você ja esperou tempo demais");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
