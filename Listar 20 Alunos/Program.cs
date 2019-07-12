07cb8fdf676ca5dfe32b9896fefc8c9948f775d9
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 16:27:10 -0300

    Listar Alunos Espaçamento

--- Listar 20 Alunos/Program.cs
+++ Listar 20 Alunos/Program.cs
@@ -27,23 +27,23 @@ namespace Listar_20_Alunos
             {"Pedro","19","Feminino"},
             {"Bruna","19","Masculino"},
             {"Maicon","21","Masculino"},
-            {"Rafael","23","Feminino"},
+            {"Rafael","31","Feminino"},
             {"Ricardo","18","Masculino"},
             {"Bruno","15","Feminino"},
             {"Serial","19","Feminino"},
-            {"Jonas","19","Masculino"},
+            {"Jonas","28","Masculino"},
             {"Udo","21","Masculino"},
             {"Erick","23","Feminino"},
             {"Gilberto","18","Masculino"},
-            {"Tomas","15","Feminino"},
+            {"Tomas","19","Feminino"},
             {"Janete","19","Feminino"},
-            {"Alecir","15","Feminino"},
+            {"Alecir","51","Feminino"},
             {"Luiz","19","Feminino"}
             };
 
             for (int i = 0; i < ListaDeAlunos.GetLength(0); i++)
             {
-                Console.WriteLine($"{i + 1}° Aluno: {ListaDeAlunos[i, 0]}, Idade: {ListaDeAlunos[i, 1]}, Sexo: {ListaDeAlunos[i, 2]} ");              
+                Console.WriteLine($"{i + 1}° Aluno: {ListaDeAlunos[i, 0]}, \tIdade: {ListaDeAlunos[i, 1]}, \tSexo: {ListaDeAlunos[i, 2]} ");              
             }
             Console.ReadKey();
         }
