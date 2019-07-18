7e23eb7cd1b884a0f288e59463050d0eec425400
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:08:44 -0300

    Dog Pronto

--- Querys/VendaCachorroQuente.sql
+++ Querys/VendaCachorroQuente.sql
@@ -1,4 +1,5 @@
-﻿select * from FoodTruck;
+﻿select * from FoodTruck
+order by TipoDog;
 
 /*
 insert into FoodTruck (TipoDog, TotalVenda, DataVenda)
@@ -6,8 +7,20 @@ values ('Dog Soya', '14', '11/03/2019');
 */
 
 select TipoDog,
-	    SUM(TotalVenda) as 'Media de Venda',
+	   Id,
+	    (SUM(TotalVenda)/ count(*)) as 'Media de Venda',
 	    MONTH(DataVenda) as 'Mes Vendido'	
 	from FoodTruck
-group by TipoDog, TotalVenda, DataVenda
-order by TotalVenda desc;
+	where Id between 2 and 3
+group by TipoDog, MONTH (DataVenda), Id
+order by 'Media de Venda' desc;
+
+/*
+select TipoDog,
+	    (SUM(TotalVenda)/ count(*)) as 'Media de Venda',
+	    MONTH(DataVenda) as 'Mes Vendido'	
+	from FoodTruck
+	where MONTH (DataVenda) between 2 and 3
+group by TipoDog, MONTH (DataVenda)
+order by 'Media de Venda' desc;
+*/
