7ebd25c80d7bd74a0547ce154eff487881192d5a
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 14:18:08 -0300

    Primeiro Inner Join

---
+++ Querys/Primeiro Inner.sql
@@ -0,0 +1,23 @@
+﻿select 
+		Lv.Id,
+		Lv.Nome,
+		IIF(Lv.Alocado = 1 , 'Sim', 'Não') as 'Alocado',
+		BL.Nome as 'Nome da Biblioteca'
+	from Biblioteca1 BL
+		inner join Livro1 LV on BL.Id= LV.Biblioteca;
+
+select
+		Bl.Nome,
+		Count(Lv.Id) as 'QuantLivros',
+		US.Nome as 'UserName'
+	from Biblioteca1 BL
+	inner join Livro1 LV on BL.ID = LV.Biblioteca
+	inner join Usuario US on LV.UserInc = US.Id
+	group by BL.nome, US.nome
+
+
+select * from Livro1;
+
+/*
+delete from Biblioteca1;
+*/
---
+++ Querys/dbo.Alunos.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Alunos] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]    VARCHAR (100) NOT NULL,
+    [Ativo]   BIT           NOT NULL,
+    [UsuInc]  INT           NOT NULL,
+    [UsuAlt]  INT           NOT NULL,
+    [DateInc] DATETIME      NOT NULL,
+    [DateAlt] DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Alunos_UsuarioInc] FOREIGN KEY ([UsuInc]) REFERENCES [dbo].[Usuario] ([Id]),
+    CONSTRAINT [FK_Alunos_UsuarioAlt] FOREIGN KEY ([UsuAlt]) REFERENCES [dbo].[Usuario] ([Id])
+);
+
