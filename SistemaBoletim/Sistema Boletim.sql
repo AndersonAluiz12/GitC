bf9fc026bba225fc80a574c0db1f4a54e9698a8d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 17:23:55 -0300

    Locação Final comitada

---
+++ SistemaBoletim/Sistema Boletim.sql
@@ -0,0 +1,8 @@
+﻿select AL.NomeALuno, 
+	   SUM(NT.Nota) / count(FR.Frequencia) as 'Média',
+	   IIF((Sum(NT.Nota) / Count (NT.Nota) >=7), 'Passou','Reprovou') as 'Passaou por Média? ',
+       IIF (((sum(convert(money,FR.Frequencia)) / convert(money, count(FR.Frequencia)*100))>75), 'Passou','Reprovou') as 'Passou por Frequecia?'
+from Alunos AL
+inner join Frequencia FR on AL.Id = FR.Aluno
+inner join Notas NT on AL.Id = NT.Aluno
+group by AL.NomeAluno
