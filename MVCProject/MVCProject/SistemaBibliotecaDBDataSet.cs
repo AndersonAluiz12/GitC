e061151d11831c93ee2b969bbd5209d2bb1a041d
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-06 17:12:31 -0300

    Save Novo Programa WPF

--- MVCProject/MVCProject/SistemaBibliotecaDBDataSet.cs
+++ MVCProject/MVCProject/SistemaBibliotecaDBDataSet.cs
@@ -12,11 +12,3 @@ namespace MVCProject.SistemaBibliotecaDBDataSetTableAdapters
     partial class LivrosTableAdapter
     {
     }
-
-    partial class EditorasTableAdapter
-    {
-    }
-
-    public partial class AutoresTableAdapter {
-    }
-}
