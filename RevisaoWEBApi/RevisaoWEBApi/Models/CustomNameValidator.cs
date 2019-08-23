85194cfe1875602885355ccf5b196aa7800de342
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-23 17:18:26 -0300

    Commit master 23/08

---
+++ RevisaoWEBApi/RevisaoWEBApi/Models/CustomNameValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RevisaoWEBApi.Models
+{
+    public class CustomNameValidator : ValidationAttribute
+    {
+            protected override ValidationResult IsValid (Object value,
+                ValidationContext validationContext)
+            {
+                if(value != null)
+                {
+                    if(value.ToString().Contains("Anderson"))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("Novo Teste");
+                }
+                }
+
+                return new ValidationResult("O Campo: " +
+                    validationContext.DisplayName
+                    + " é um campo obrigatório");
+            }
+    }
+}
